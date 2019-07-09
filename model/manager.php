344d1e6807eb7c3ed5bba44ea88e9f34e3320750
jeremie <sallesjry@gmail.com>
2019-07-09 13:24:11 +0200

    mise en place du site

---
+++ model/manager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ */
+
+class Manager
+{
+
+  protected function dbconnect()
+  {
+    try
+      {
+        $db = new \PDO('mysql:host=localhost;dbname=JeanForteroche;charset=utf8', 'root', 'root');
+        return $db;
+      }
+        catch(Exception $e)
+      {
+        die('Erreur : '.$e->getMessage());
+      }
+  }
+}
---
+++ view/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!--feuille de style-->
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title><?php $title ?></title>
+  </head>
+  <body>
+    <?php $content ?>
+  </body>
+</html>
